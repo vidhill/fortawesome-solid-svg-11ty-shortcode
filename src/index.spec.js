1369dde2478e565a128291f01e3d8dc56bc07b35
David Hill <dh366k@us.att.com>
2021-06-22 19:42:05 +0100

    Update unit tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,22 +1,32 @@
-import { fortawesomeBrandsShortcode, fortawesomeBrandsPlugin } from './index';
+import { fortawesomeSolidShortcode, fortawesomeSolidPlugin } from './index';
 
-describe("test 'fortawesomeBrandsShortcode()'", () => {
+describe("test 'fortawesomeSolidShortcode()'", () => {
     it('should be a defined function', () => {
-        expect(fortawesomeBrandsShortcode).not.toBeUndefined();
-        expect(fortawesomeBrandsShortcode).toBeType('function');
+        expect(fortawesomeSolidShortcode).not.toBeUndefined();
+        expect(fortawesomeSolidShortcode).toBeType('function');
     });
 
     it('should return a defined string', () => {
-        const result = fortawesomeBrandsShortcode('github', 'social-icon');
+        const result = fortawesomeSolidShortcode('bell');
         expect(result).not.toBeUndefined();
         expect(result).toBeType('string');
     });
+
+    it('should return a non-empty string', () => {
+        const result = fortawesomeSolidShortcode('bell');
+        expect(result.length).not.toBe(0);
+    });
+
+    it('should return a string including the passed class name', () => {
+        const result = fortawesomeSolidShortcode('bell', 'social-icon');
+        expect(result).toContain('social-icon');
+    });
 });
 
-describe("test 'fortawesomeBrandsPlugin()'", () => {
+describe("test 'fortawesomeSolidPlugin()'", () => {
     it('should be a defined function', () => {
-        expect(fortawesomeBrandsPlugin).not.toBeUndefined();
-        expect(fortawesomeBrandsPlugin).toBeType('function');
+        expect(fortawesomeSolidPlugin).not.toBeUndefined();
+        expect(fortawesomeSolidPlugin).toBeType('function');
     });
 
     describe('test the plugin', () => {
@@ -30,15 +40,15 @@ describe("test 'fortawesomeBrandsPlugin()'", () => {
         });
 
         it("should call 'eleventyConfig.addShortcode()'", () => {
-            fortawesomeBrandsPlugin(eleventyConfig);
+            fortawesomeSolidPlugin(eleventyConfig);
             expect(mockAddShortcode).toBeCalledTimes(1);
         });
 
         it("should call 'eleventyConfig.addShortcode()' with the expected arguments", () => {
-            fortawesomeBrandsPlugin(eleventyConfig);
+            fortawesomeSolidPlugin(eleventyConfig);
             expect(mockAddShortcode).toBeCalledWith(
-                'fortawesomeBrand',
-                fortawesomeBrandsShortcode
+                'fortawesomeSolid',
+                fortawesomeSolidShortcode
             );
         });
     });
